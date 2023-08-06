fb21234c7e1b35213ac2e03d3ae707387a87869d
sami12344 <freelancersami16203@gmail.com>
2023-08-07 00:40:06 +0600

    First commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// src/app.ts
+import express, { Request, Response } from 'express'
+import * as dotenv from 'dotenv'
+import { twilioMiddleware } from './middleware/twilioMiddleware'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Add the Twilio middleware to a specific route
+app.post('/phone', twilioMiddleware, (req: Request, res: Response) => {
+  res.send('Hello from Express with Twilio middleware! To get message post "phone" in json in route phone')
+})
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('SMS IS SENT!')
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
---
+++ src/middleware/twilioMiddleware.ts
@@ -0,0 +1,57 @@
+// middleware/twilioMiddleware.ts
+import { Request, Response, NextFunction } from 'express'
+import twilio from 'twilio'
+import * as dotenv from 'dotenv';
+
+
+dotenv.config()
+
+// Twilio credentials
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER
+
+if (!accountSid || !authToken || !twilioPhoneNumber) {
+  throw new Error(
+    'TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_PHONE_NUMBER must be set in .env file.'
+  )
+}
+
+// Create a Twilio client
+const client = twilio(accountSid, authToken)
+
+// Twilio middleware function
+export function twilioMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+
+ const {phone} = req.body
+  // Custom logic to decide when to send an SMS
+  const shouldSendSMS = true // Replace this with your own condition
+
+  if (shouldSendSMS) {
+    const phoneNumber = phone// Replace with the recipient's phone number
+    const message = 'Hello from Twilio middleware!'
+
+    // Send SMS using Twilio
+    client.messages
+      .create({
+        body: message,
+        from: twilioPhoneNumber,
+        to: phoneNumber,
+      })
+      .then((message) => {
+        console.log(`SMS sent with SID: ${message.sid}`)
+        next()
+      })
+      .catch((error) => {
+        console.error(`Error sending SMS: ${error.message}`)
+        next()
+      })
+  } else {
+    // If you don't want to send an SMS, just proceed to the next middleware
+    next()
+  }
+}
